aae09b7ad52271c0fb10c91563cca3de4f5a498b
vitor.harissis <vitor.harissis@lbca.com.br>
2026-02-03 23:55:16 -0300

    build(docker): disable API server startup in entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 set -e
 
-# Start Telegram bot in background
-poetry run python scripts/telegram_bot.py &
-
-# Start API in foreground
-exec poetry run uvicorn src.api.main:app --host 0.0.0.0 --port "${PORT:-8000}"
+# Start Telegram bot in foreground
+exec poetry run python scripts/telegram_bot.py
