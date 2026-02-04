2f0fc0c3e1ea5b4aedd1c3294c4684d63fcb8101
vitor.harissis <vitor.harissis@lbca.com.br>
2026-02-04 09:17:39 -0300

    build(docker): start FastAPI service in entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -e
 
+# Start FastAPI in background
+poetry run uvicorn src.api.main:app --host 0.0.0.0 --port ${PORT:-8000} &
+
 # Start Telegram bot in foreground
 exec poetry run python scripts/telegram_bot.py
